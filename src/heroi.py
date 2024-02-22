7da3d2ef07621da5082c5e02a3f529ebeed6c9ec
mabdsouza@outlook.com <mabdsouza@outlook.com>
2024-02-22 09:43:13 -0300

    quinto commit

---
+++ src/heroi.py
@@ -0,0 +1,40 @@
+nickname = "nome heroi"
+qtd_XP = 1000
+elo_heroi = ""
+
+if qtd_XP == 1000:
+    elo_heroi = "Ferro"
+elif 1001 >= qtd_XP <= 2000:
+    elo_heroi = "Bronze"
+elif 2001 >= qtd_XP <= 6000:
+    elo_heroi = "Prata"
+elif 6001 >= qtd_XP <= 7000:
+    elo_heroi = "Ouro"
+elif 7001 >= qtd_XP <= 8000:
+    elo_heroi = "Platina"
+elif 8001 >= qtd_XP <= 9000:
+    elo_heroi = "Ascendente"
+elif 9001 >= qtd_XP <= 10000:
+    elo_heroi = "Imortal"
+elif qtd_XP >= 10001:
+    elo_heroi = "Radiante"
+
+match elo_heroi:
+    case "Ferro":
+        print(f"O heroi de nome {nickname} está no nível {elo_heroi}")
+    case "Bronze":
+        print(f"O heroi de nome {nickname} está no nível {elo_heroi}")
+    case "Prata":
+        print(f"O heroi de nome {nickname} está no nível {elo_heroi}")
+    case "Ouro":
+        print(f"O heroi de nome {nickname} está no nível {elo_heroi}")
+    case "Platina":
+        print(f"O heroi de nome {nickname} está no nível {elo_heroi}")
+    case "Ascendente":
+        print(f"O heroi de nome {nickname} está no nível {elo_heroi}")
+    case "Imortal":
+        print(f"O heroi de nome {nickname} está no nível {elo_heroi}")
+    case "Radiante":
+        print(f"O heroi de nome {nickname} está no nível {elo_heroi}")
+    case _:
+        print("valor invalido")
